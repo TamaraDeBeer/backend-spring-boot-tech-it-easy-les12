ab8c8c7f4b43ea9e44582747d6f49052a889f77a
TamaraDeBeer <tamara.debeer@hotmail.com>
2023-08-29 17:05:30 +0200

    huiswerkopdracht les 10

---
+++ src/main/java/com/example/backendspringboottechiteasycontrollerles10/BackendSpringBootTechItEasyControllerLes10Application.java
@@ -0,0 +1,13 @@
+package com.example.backendspringboottechiteasycontrollerles10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringBootTechItEasyControllerLes10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringBootTechItEasyControllerLes10Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendspringboottechiteasycontrollerles10/BackendSpringBootTechItEasyControllerLes10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendspringboottechiteasycontrollerles10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringBootTechItEasyControllerLes10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
