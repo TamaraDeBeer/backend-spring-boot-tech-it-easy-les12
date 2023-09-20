d98d1729323c9f24619827feeab03ae3496cac7c
TamaraDeBeer <tamara.debeer@hotmail.com>
2023-09-20 12:13:22 +0200

    models voor wall bracket, remote control en ci modules compleet met constructor en getters en setters

--- src/main/java/com/example/backendspringboottechiteasycontrollerles10/models/CiModule.java
+++ src/main/java/com/example/backendspringboottechiteasycontrollerles10/models/CiModule.java
@@ -1,4 +1,58 @@
 package com.example.backendspringboottechiteasycontrollerles10.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "ci_modules")
 public class CiModule {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String type;
+    private Double price;
+
+    public CiModule(Long id, String name, String type, Double price) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+
+    public CiModule(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
--- src/main/java/com/example/backendspringboottechiteasycontrollerles10/models/WallBracket.java
+++ src/main/java/com/example/backendspringboottechiteasycontrollerles10/models/WallBracket.java
@@ -1,4 +1,69 @@
 package com.example.backendspringboottechiteasycontrollerles10.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "wall_brackets")
 public class WallBracket {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String size;
+    private Boolean adjustable;
+    private Double price;
+
+    public WallBracket(Long id, String name, String size, Boolean adjustable, Double price) {
+        this.id = id;
+        this.name = name;
+        this.size = size;
+        this.adjustable = adjustable;
+        this.price = price;
+    }
+
+    public WallBracket() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Boolean getAdjustable() {
+        return adjustable;
+    }
+
+    public void setAdjustable(Boolean adjustable) {
+        this.adjustable = adjustable;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
