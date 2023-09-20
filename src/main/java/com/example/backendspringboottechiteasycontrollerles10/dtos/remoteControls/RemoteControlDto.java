e884489fa26faa7374f59e1b6f351f4f43878c94
TamaraDeBeer <tamara.debeer@hotmail.com>
2023-09-20 12:24:27 +0200

    dtos en inputDtos gemaakt voor ciModule, wallBracket en remoteControl

--- src/main/java/com/example/backendspringboottechiteasycontrollerles10/dtos/remoteControls/RemoteControlDto.java
+++ src/main/java/com/example/backendspringboottechiteasycontrollerles10/dtos/remoteControls/RemoteControlDto.java
@@ -1,4 +1,70 @@
 package com.example.backendspringboottechiteasycontrollerles10.dtos.remoteControls;
 
+import jakarta.validation.Valid;
+
+@Valid
 public class RemoteControlDto {
+    public Long id;
+    public String name;
+    public String brand;
+    public String batteryType;
+    public Double price;
+    public Integer originalStock;
+    public String compatibleWith;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBatteryType() {
+        return batteryType;
+    }
+
+    public void setBatteryType(String batteryType) {
+        this.batteryType = batteryType;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getOriginalStock() {
+        return originalStock;
+    }
+
+    public void setOriginalStock(Integer originalStock) {
+        this.originalStock = originalStock;
+    }
+
+    public String getCompatibleWith() {
+        return compatibleWith;
+    }
+
+    public void setCompatibleWith(String compatibleWith) {
+        this.compatibleWith = compatibleWith;
+    }
 }
